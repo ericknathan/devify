4d035ec61b8642ad5550c570ce645b4f41de6245
ericknathan <erick.capito@hotmail.com>
2023-04-05 20:33:16 -0300

    feat: add title on play button

--- src/lib/components/TrackControls.svelte
+++ src/lib/components/TrackControls.svelte
@@ -4,7 +4,7 @@
 
 <div class="controls-wrapper">
 	<button><img src="/icons/prev.svg" alt="" /></button>
-	<a href={trackUrl}><img src="/icons/play.svg" alt="" /></a>
+	<a href={trackUrl} title="Visualizar música no LastFM"><img src="/icons/play.svg" alt="" /></a>
 	<button><img src="/icons/next.svg" alt="" /></button>
 </div>
 
